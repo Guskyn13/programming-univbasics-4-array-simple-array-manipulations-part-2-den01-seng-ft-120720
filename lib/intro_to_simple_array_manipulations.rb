b278e9d29f87f255750eff2847bb91d716aee29c
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-30 01:47:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_concat(collection, array)
   collection == "my_favorite_things"
   array == "more_favs"
-["my_favorite_things"].concat(["more_favs"])
+["collection"].concat(["array"])
 end
