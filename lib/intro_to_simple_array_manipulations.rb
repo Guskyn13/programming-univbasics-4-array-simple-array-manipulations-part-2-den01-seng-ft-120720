d2175259c9934e59c97772d5569098d0722350e0
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-30 01:52:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_concat(collection, array)
   collection == ["raindrops on roses", "whiskers on kittens"]
   array == ["sports cars", "flatiron school"]
-["collection"].concat(["array"])
+["raindrops on roses", "whiskers on kittens"].concat(["sports cars", "flatiron school"])
 end
